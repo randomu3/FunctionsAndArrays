93d3bb9a9e942aefcc5aed828c20b0c0d59a62e6
randomu3 <demiz99@mail.ru>
2024-03-27 20:55:07 +0500

    groups types

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -5,38 +5,35 @@
 
 using namespace std;
 
+// Функции для работы с массивами типа int
 void FillRand(int arr[], const int n, const int max = DEFAULT_MAX_VALUE);
-void FillRand(double arr[], const int n, const double max = DEFAULT_MAX_VALUE);
-void FillRand(char arr[], const int n);
-
 void Print(const int arr[], const int n);
-void Print(const double arr[], const int n);
-void Print(const char arr[], const int n);
-
 int Sum(const int arr[], const int n);
-double Sum(const double arr[], const int n);
-int Sum(const char arr[], const int n);
-
 double Avg(const int arr[], const int n);
-double Avg(const double arr[], const int n);
-double Avg(const char arr[], const int n);
-
 int minValueIn(const int arr[], const int n);
-double minValueIn(const double arr[], const int n);
-char minValueIn(const char arr[], const int n);
-double minValueIn(const double arr[], const int n);
-
 int maxValueIn(const int arr[], const int n);
-double maxValueIn(const double arr[], const int n);
-char maxValueIn(const char arr[], const int n);
-
 void shiftLeft(int arr[], const int n, int shift);
-void shiftLeft(double arr[], const int n, int shift);
-void shiftLeft(char arr[], const int n, int shift);
-
 void shiftRight(int arr[], const int n, int shift);
-void shiftRight(char arr[], const int n, int shift);
+
+// Функции для работы с массивами типа double
+void FillRand(double arr[], const int n, const double max = DEFAULT_MAX_VALUE);
+void Print(const double arr[], const int n);
+double Sum(const double arr[], const int n);
+double Avg(const double arr[], const int n);
+double minValueIn(const double arr[], const int n);
+double maxValueIn(const double arr[], const int n);
+void shiftLeft(double arr[], const int n, int shift);
 void shiftRight(double arr[], const int n, int shift);
+
+// Функции для работы с массивами типа char
+void FillRand(char arr[], const int n);
+void Print(const char arr[], const int n);
+int Sum(const char arr[], const int n);
+double Avg(const char arr[], const int n);
+char minValueIn(const char arr[], const int n);
+char maxValueIn(const char arr[], const int n);
+void shiftLeft(char arr[], const int n, int shift);
+void shiftRight(char arr[], const int n, int shift);
 
 int main() {
     const int n = 10;
